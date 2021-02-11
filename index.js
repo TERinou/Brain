94ce4c310b5834e43cb4ba8ae22d8ab80a3d3cdc
Dorpaxio <dorpaxione@gmail.com>
2021-02-11 18:06:16 +0100

    Revert "Feature/get key words"

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const gkw = require('./functions/getKeyWords');
-
-//console.log(gkw.getKeyWord("C'est quoi une chaise."));
-
-module.exports = {
-  getToken: gkw.getToken,
-  getPosTag: gkw.getPosTag,
-  getLemmas: gkw.getLemmas,
-  getKeyWork: gkw.getKeyWord
-};
+exports.example = function () {
+    console.log(`Une fonction d'exemple !`);
+}
