277e8a5d1e1ff6fb07c206f6e235c81c32ef5145
Gracia-Moulis Kevin <kevin.gracia-moulis@etu.umontpellier.fr>
2021-02-11 17:05:23 +0100

    ajout getKeyWords

--- functions/getKeyWords.js
+++ functions/getKeyWords.js
@@ -1,12 +1,23 @@
 const NlpjsTFr = require('nlp-js-tools-french');
 
+// tagTypes: ["adj", "adv", "art", "con", "nom", "ono", "pre", "ver", "pro"]
+// UNK si inconnu
 const config = {
     strictness: false,
     minimumLength: 3,
     debug: true
 };
 
+/**
+ * Différentes fonctions d'extraction à partir d'une phrase
+ * @type {{getLemmas: (function(*=): *), getKeyWords: (function(*=): []), getToken: (function(*=): *), getPosTag: (function(*=): *)}}
+ */
 module.exports = {
+    /**
+     * Tokenize les mots de la phrase
+     * @param phrase
+     * @returns Tableau des mots.
+     */
     getToken: function (phrase){
         if (phrase == null)
             phrase = "";
@@ -17,6 +28,11 @@ module.exports = {
 
         return tokenizedWords;
     },
+    /**
+     * Definit le type des mots (ex: nourrir => verbe)
+     * @param phrase
+     * @returns Tableau de dictionnaire contenant id,word,pos de chaque mot
+     */
     getPosTag: function (phrase){
         if (phrase == null)
             phrase = "";
@@ -27,6 +43,11 @@ module.exports = {
 
         return posTaggedWords;
     },
+    /**
+     * Retourne le mot au dessus (ex: semble => sembler)
+     * @param phrase
+     * @returns Tableau de dictionnaire contenant id,word,lemma de chaque mot
+     */
     getLemmas: function (phrase){
         if (phrase == null)
             phrase = "";
@@ -36,5 +57,22 @@ module.exports = {
         const lemmatizedWords = nlpToolsFr.lemmatizer();
 
         return lemmatizedWords;
+    },
+    /**
+     * Extrait les mots clefs de la phrase
+     * @param phrase
+     * @returns Liste des mots clefs
+     */
+    getKeyWords: function (phrase){
+        let keyWords = [];
+
+        let posTaggedWords = this.getPosTag(phrase);
+
+        posTaggedWords.forEach(function (item, index, array){
+            keyWords.push(item.word);
+        });
+
+        console.log("[+] " + keyWords);
+        return keyWords;
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const gkw = require('./functions/getKeyWords');
 
+//gkw.getKeyWords("La pomme est un fruit.");
+
 module.exports = gkw.getToken;
 module.exports = gkw.getPosTag;
 module.exports = gkw.getLemmas;
+module.exports = gkw.getKeyWords;
